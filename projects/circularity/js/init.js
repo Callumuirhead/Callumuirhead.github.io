38407e8a5d86ae723fa29ad45553ed57a517c6c7
Callum M <cm6489915@lpssonline.com>
2025-02-27 17:03:31 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -30,11 +30,9 @@ var init = function (window) {
             circles.push(circle)
         }
         // TODO 3 / 7 : Call the drawCircle() function 
-        drawCircle()
-        drawCircle()
-        drawCircle()
-        drawCircle()
-        drawCircle()
+       for (var completeCircles = 0; completeCircles < 100; completeCircles++) {
+        drawCircle(circles[completeCircles])
+       }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -47,17 +45,12 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0])
-            physikz.updatePosition(circles[1])
-            physikz.updatePosition(circles[2])
-            physikz.updatePosition(circles[3])
-            physikz.updatePosition(circles[4])
+            for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i])
+                game.checkCirclePosition(circles[i])
+            }
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(circles[0])
-            game.checkCirclePosition(circles[1])
-            game.checkCirclePosition(circles[2])
-            game.checkCirclePosition(circles[3])
-            game.checkCirclePosition(circles[4])
+           
             // TODO 9 : Iterate over the array
            
             
@@ -68,12 +61,12 @@ var init = function (window) {
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
-       var rightEdge = circle.x + circle.radius
-       var leftEdge = circle.x - circle.radius
-       var topEdge = circle.y - circle.radius
-       var bottomEdge = circle.y + circle.radius 
+       
         game.checkCirclePosition = function(circle) {
-
+        var rightEdge = circle.x + circle.radius;
+        var leftEdge = circle.x - circle.radius;
+        var topEdge = circle.y - circle.radius;
+        var bottomEdge = circle.y + circle.radius ;
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
             if ( leftEdge > canvas.width ) {
                 circle.x = 0;
